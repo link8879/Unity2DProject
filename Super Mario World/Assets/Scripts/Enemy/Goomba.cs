1277a3f146ca92b2fffe9bd03b39ea0ee1b215ce
WonHyung Lee(Gram) <link8879@naver.com>
2025-06-06 22:42:39 +0900

    굼바 벽 플립 구현

--- Super Mario World/Assets/Scripts/Enemy/Goomba.cs
+++ Super Mario World/Assets/Scripts/Enemy/Goomba.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Goomba : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] float moveSpeed = -1f;
     Rigidbody2D rb;
 
     void Start()
@@ -12,8 +12,22 @@ public class Goomba : MonoBehaviour
 
     void Update()
     {
-        Vector2 velocity = rb.linearVelocity;
-        velocity.x = moveSpeed;
-        rb.linearVelocity = new Vector2(velocity.x, velocity.y);
+        rb.linearVelocity = new Vector2(moveSpeed, rb.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing();
+    }
+
+    void FlipEnemyFacing()
+    {
+        Debug.Log(Mathf.Sign(rb.linearVelocity.x));
+        transform.localScale = new Vector2((Mathf.Sign(rb.linearVelocity.x)), 1f);
     }
 }
