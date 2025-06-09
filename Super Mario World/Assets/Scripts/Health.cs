8d59fb162f02590d45ad7dd444010f56c83279cf
Rungbam(main) <marungbam@naver.com>
2025-06-09 21:32:46 +0900

    충돌체크 크기 조절 완

--- Super Mario World/Assets/Scripts/Health.cs
+++ Super Mario World/Assets/Scripts/Health.cs
@@ -10,11 +10,19 @@ public class Health : MonoBehaviour
     private int life = 10;
     private int health = 1;
     public Animator playerAnimator;
+    public BoxCollider2D headCollider;
+    public BoxCollider2D feetCollider;
+    public BoxCollider2D leftCollider;
+    public BoxCollider2D rightCollider;
 
 
     void Start()
     {
         playerAnimator = GetComponent<Animator>();
+        headCollider = transform.Find("HeadCollider").GetComponent<BoxCollider2D>();
+        feetCollider = transform.Find("FeetCollider").GetComponent<BoxCollider2D>();
+        leftCollider = transform.Find("LeftCollider").GetComponent<BoxCollider2D>();
+        rightCollider = transform.Find("RightCollider").GetComponent<BoxCollider2D>();
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -49,5 +57,11 @@ public class Health : MonoBehaviour
     public void Transforming()
     {
         playerAnimator.runtimeAnimatorController = BigMario;
+
+        leftCollider.size = new Vector2(0.48f, 1.2f);
+        rightCollider.size = new Vector2(0.48f, 1.4f);
+
+        headCollider.offset = new Vector2(0f, 0.75f);
+        feetCollider.offset = new Vector2(0f, -0.75f);
     }
 }
